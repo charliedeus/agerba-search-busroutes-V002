60e43a0d08b29a91be8fe4dcd24ab7833304b69a
Charles Loureiro de Deus <charliedeus@gmail.com>
2023-06-28 11:41:05 -0300

    feat:add responsive layout to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import Link from 'next/link'
 
 export default async function Home() {
   return (
-    <main className="relative inset-0 flex h-screen w-full">
+    <main className="relative inset-0 flex h-screen w-full laptop:mx-auto laptop:max-w-[1280px]">
       <div className="relative flex h-full w-full">
         <Image
           src={'/bg-02.jpg'}
@@ -18,10 +18,10 @@ export default async function Home() {
         <div className="absolute inset-0 z-10 bg-gradient-to-b from-transparent to-black" />
       </div>
 
-      <div className="absolute inset-0 z-20 mt-auto flex h-1/2 flex-col justify-between gap-4 p-8">
-        <Image src={logoImg} alt="" width={120} />
+      <div className="absolute inset-0 z-20 mt-auto flex h-1/2 flex-col justify-between gap-4 p-8 laptop:w-2/3 laptop:justify-normal laptop:gap-8">
+        <Image src={logoImg} alt="" className="w-32 laptop:w-64" />
 
-        <p className="text-base text-zinc-100">
+        <p className="text-base text-zinc-100 laptop:text-xl">
           Planeje suas viagens com facilidade! Nosso aplicativo permite que você
           pesquise horários atualizados das linhas de transporte, garantindo que
           você chegue ao seu destino no tempo certo. Simplifique sua jornada com
@@ -30,7 +30,7 @@ export default async function Home() {
 
         <Link
           href={'/linhas'}
-          className="flex items-center justify-between gap-2 rounded-md bg-zinc-50 px-4 py-3"
+          className="flex items-center justify-between gap-2 rounded-md bg-zinc-50 px-4 py-3 laptop:w-1/2"
         >
           <span className="font-bold text-zinc-950">Consultar</span>
           <ArrowRight className="h-4 w-4" />
